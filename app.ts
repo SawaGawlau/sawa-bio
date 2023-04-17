0beccd60f1a906ad3f3e3b8c2cafb56003e8c43b
SawaGawlau <sawa.gawlau@gmail.com>
2023-04-17 14:35:14 +0200

    fix: workflow

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import express, { Application, Request, Response } from 'express'
-import './middleware/dotenv'
+// import './middleware/dotenv'
 import path from 'path'
 
 const app: Application = express()
