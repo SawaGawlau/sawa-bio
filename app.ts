cd7c635418801c2bb4867447de7eb60e4eb158f3
SawaGawlau <sawa.gawlau@gmail.com>
2023-04-18 09:43:21 +0200

    fix: routing test

--- app.ts
+++ app.ts
@@ -7,8 +7,12 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.static(path.join(path.resolve(), './public')))
 
-app.get('/hello', (req: Request, res: Response) => {
-  return res.send('Hello world')
+app.get('/', (req: Request, res: Response) => {
+  return res.send('Hello on sawa-bio testing app')
+})
+
+app.get('/sawa', (req: Request, res: Response) => {
+  return res.send('Hello Sawa!')
 })
 
 const port: number = 8080
